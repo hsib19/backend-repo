d6e4e064d49871d12d07714a94f5c7729bbf7a0c
Hasib Muharam <hasibmuha@gmail.com>
2024-12-13 11:33:07 +0700

    xzz

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -1,9 +1,15 @@
 import admin from 'firebase-admin';
 
-const serviceAccount = require('../serviceAccountKey.json'); // Replace with your service account key file name
+const firebaseConfig = {
+    apiKey: "AIzaSyBVgnGO91QQYB6mhFuB6AmyzFY6OkOmcDY",
+    authDomain: "ebuddy-app.firebaseapp.com",
+    projectId: "ebuddy-app",
+    storageBucket: "ebuddy-app.firebasestorage.app",
+    messagingSenderId: "154408355949",
+    appId: "1:154408355949:web:06ea1e316ef9782718d764",
+    measurementId: "G-GQBVTL1VK9"
+};
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+admin.initializeApp(firebaseConfig);
 
 export default admin;
